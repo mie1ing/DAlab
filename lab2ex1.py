3a5dd135b1af344ece70f8a4b5a6d05b649b638d
Chengyuan Zhang <chengyuan.zhang@studium.uni-hamburg.de>
2025-04-15 17:31:45 +0200

    week2.1.3 repeat for compare time

--- lab2ex1.py
+++ lab2ex1.py
@@ -21,30 +21,33 @@ def sum_numba_parallel(x):
         result += ((-1)**(i + 1)) / (i * np.exp2(i))
     return result
 
-n = 1000
+n = 200
+repeat = 10000
 
 sttime1 = time.time()
-result1 = sum_cycle(n)
+for _ in range(repeat):
+    result1 = sum_cycle(n)
 edtime1 = time.time()
 delta1 = edtime1 - sttime1
 
 sttime2 = time.time()
-result2 = sum_vector(n)
+for _ in range(repeat):
+    result2 = sum_vector(n)
 edtime2 = time.time()
 delta2 = edtime2 - sttime2
 
-sum_numba_parallel(10) # warm up
-
+sum_numba_parallel(n) # warm up
 sttime3 = time.time()
-result3 = sum_numba_parallel(n)
+for _ in range(repeat):
+    result3 = sum_numba_parallel(n)
 edtime3 = time.time()
 delta3 = edtime3 - sttime3
 
 print(delta1)
 print(delta2)
 print(delta3)
-print(f'Vectorization time / Loop time for n = {n}: {delta2 / delta1}')
-print(f'Numba time / Loop time for n = {n}: {delta3 / delta1}')
+print(f'Vectorization time / Loop time for n = {n} repeat {repeat} times: {delta2 / delta1}')
+print(f'Numba time / Loop time for n = {n} repeat {repeat} times: {delta3 / delta1}')
 
 n_for_plot = np.arange(1, n + 1, 1)
 snarray = [sum_vector(i) for i in n_for_plot]
