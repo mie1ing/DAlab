ac6fb953c896c493cf8ebdd9b5bbcd479c0bbbae
Chengyuan Zhang <chengyuan.zhang@studium.uni-hamburg.de>
2025-04-15 17:13:37 +0200

    week2.1.2 move plotting to the end

--- lab1ex2plot.py
+++ lab1ex2plot.py
@@ -14,6 +14,12 @@ file = open("kdvsol.dat","rb")
 sol = np.load(file)
 file.close()
 
+crests_start = argrelmax(sol[0,:])
+crests_end = argrelmax(sol[-1,:])
+
+phase_speed = (x[crests_end[0][0]] - x[crests_start[0][0]]) / (t[-1] - t[0])
+print(f'Phase Speed: {phase_speed}')
+
 X, Y = np.meshgrid(x, t)
 plt.pcolormesh(X, Y, sol)
 plt.colorbar()
@@ -22,9 +28,3 @@ plt.ylabel('t')
 plt.title('Korteveg de Vries on a Periodic Domain')
 plt.savefig('overleaf/67fe68e723632af9fad1411b/figures/lab1ex2.png')
 plt.close()
-
-crests_start = argrelmax(sol[0,:])
-crests_end = argrelmax(sol[-1,:])
-
-phase_speed = (x[crests_end[0][0]] - x[crests_start[0][0]]) / (t[-1] - t[0])
-print(f'Phase Speed: {phase_speed}')
